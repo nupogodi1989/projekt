43c95e19e4c475e85098a6ca8c616398a9ffffe1
boris <basarkis@alumni.unca.edu>
2020-08-22 23:13:36 -0400

    update routes.rb for User and registrations and confirmations controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
+
+  devise_for :users, controllers: {
+      registrations: 'registrations',
+      confirmations: 'confirmations'
+  }
   root 'home#index'
 
 end
